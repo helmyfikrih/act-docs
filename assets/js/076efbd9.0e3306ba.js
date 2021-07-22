78ec621527effbd3814fbe3e827c3bddab56dcec
Helmy Fikri Husein <helmyfikrih@gmail.com>
2021-07-22 20:06:06 +0700

    build

---
+++ assets/js/076efbd9.0e3306ba.js
@@ -0,0 +1,1 @@
+(self.webpackChunkact=self.webpackChunkact||[]).push([[684],{8549:function(a){"use strict";a.exports=JSON.parse('{"allTagsPath":"/act-docs/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/act-docs/blog/tags/hello"}')}}]);
---
+++ assets/js/1dff807a.f0ebbb24.js
@@ -0,0 +1,1 @@
+(self.webpackChunkact=self.webpackChunkact||[]).push([[686],{9876:function(e){"use strict";e.exports=JSON.parse('{"permalink":"/act-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
